322817c44e2c2efa38dd79179a4b9f5849a50de0
hiron.roy <hiron.roy@341ec060-d5ea-11de-8017-1f6ee2e63914>
2009-11-20 16:02:24 +0000

    Created project.

---
+++ trunk/Generator/llGenerator/LL1Generator.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+package llGenerator;
+
+/**
+ * @author Hiron
+ *
+ */
+public class LL1Generator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main Entry Point");
+	}
+
+}
