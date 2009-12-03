1fa02bd54819a27a130f855ee79bc4aea86bfec9
mpnolan <mpnolan@341ec060-d5ea-11de-8017-1f6ee2e63914>
2009-12-03 02:58:03 +0000

    Rewrote big parts of the class. Added printHTMLTable().

--- Generator/parser3240/ParsingTable.java
+++ Generator/parser3240/ParsingTable.java
@@ -10,9 +10,11 @@ import java.util.*;
  * @author mpn
  */
 public class ParsingTable {
-    HashMap<String,ArrayList<ProductionRule>>[][] table; //2d array that will hold hashtable productionrules as entries
+    //HashMap<String,ArrayList<ProductionRule>>[][] table; //2d array that will hold hashtable productionrules as entries
+    HashMap<Nonterminal, HashMap<Token, ProductionRule>> table;
     private int rows;
-    private int columns,largestEntrySize,largestNonterminal;
+    private int columns, largestEntrySize;
+    //private int columns,largestEntrySize,largestNonterminal;
     ArrayList<Token> tokens;
     ArrayList<Nonterminal> nonTerms;
     
@@ -23,8 +25,9 @@ public class ParsingTable {
         columns = numTokens;
         tokens = myTokens;
         nonTerms = myNonTerms;
-        table = (HashMap<String,ArrayList<ProductionRule>>[][]) new HashMap[numTokens][numNonterminals];
-        for(int x=0;x<columns;x++)
+        //table = (HashMap<String,ArrayList<ProductionRule>>[][]) new HashMap[numTokens][numNonterminals];
+        table = new HashMap<Nonterminal, HashMap<Token, ProductionRule>>();
+        /**for(int x=0;x<columns;x++)
         {
             if(largestNonterminal < nonTerms.get(x).getName().length())
             {
@@ -36,12 +39,16 @@ public class ParsingTable {
                 table[x][y] = new HashMap<String,ArrayList<ProductionRule>>();
                 table[x][y].put(myNonTerms.get(y).getName()+","+myTokens.get(x).getName(),new ArrayList<ProductionRule>());
             }
-        }
+        }**/
         
     }
     // add rule P to M[A, a] in the table
     public void addEntry(Nonterminal A, Token a, ProductionRule P) {
-        String cordinate = A.getName()+","+a.getName(); //the entry[A,a] in table
+        if (table.get(A) == null)
+            table.put(A, new HashMap<Token, ProductionRule>());
+        table.get(A).put(a, P);
+        
+        /**String cordinate = A.getName()+","+a.getName(); //the entry[A,a] in table
         int numRules;   //use these to figure out space padding for printing the table
         //iterate to entry [A,a] in table and add the production rule P to that entry
         for(int x=0;x<columns;x++)
@@ -65,17 +72,20 @@ public class ParsingTable {
                     //System.out.println("Table entry is now: " + table[x][y].get(cordinate));
                 }
             }
-           
-        }
+        }**/
+
         return;
     }
     
     public ProductionRule getEntry(Nonterminal N, Token T) {
-        return table[tokens.indexOf(T)][nonTerms.indexOf(N)].get(N+","+T).get(0);
+        HashMap<Token, ProductionRule> tableRow = table.get(N);
+        if (tableRow == null)
+            return null;
+        return tableRow.get(T);
     }
     
     //get private variable table
-    public HashMap<String,ArrayList<ProductionRule>>[][] getTable()
+    public HashMap<Nonterminal, HashMap<Token, ProductionRule>> getTable()
     {
         return table;
     }
@@ -83,7 +93,16 @@ public class ParsingTable {
     //printing method to display table in a grid, first row is terminals and first column is nonterminals
     public void printTable()
     {
-        String nontermHeader,terminalHeader,rowRule;
+        System.out.println("Parsing Table");
+        System.out.println("(one entry at a time)");
+        // HashMap<Nonterminal, HashMap<Token, ProductionRule>>
+        for (Nonterminal N : table.keySet()) {
+            for (Token T : table.get(N).keySet()) {
+                System.out.println(N+","+T + " = " + table.get(N).get(T));
+            }
+        }
+        
+        /**String nontermHeader,terminalHeader,rowRule;
         largestEntrySize = largestEntrySize + 2*columns-2;
         //print the row of terminals
         System.out.print(String.format("%1$-" + largestNonterminal + "s"," ") + "  ");
@@ -113,18 +132,39 @@ public class ParsingTable {
                 
             }
             System.out.println();
-        }
+        }**/
     }
 
-    /**public void printHTMLTable()
+    public void printHTMLTable()
     {
-        String html = "<table>";
+        String html = "<html><head><title>Parsing Table</title></head><body><table border=1>";
+        html += "<tr><td>M[N,T]</td>";
+        // print the list of tokens across the top row
+        for (Token T : tokens)
+            html += "<td>" + T + "</td>";
+        html += "</tr>";
         
-        for (int y = 0; y < rows; y++) {
-            for (int x = 0; x < columns; x++) {
-                
-                
+        for (Nonterminal N : table.keySet()) {
+            html += "<tr>";
+            // the first entry in this row is the nonterminal for the row
+            html += "<td>" + fixHTML(N.toString()) + "</td>";
+            // now we print every entry in the current row
+            for (Token T : table.get(N).keySet()) {
+                html += "<td>" + fixHTML(table.get(N).get(T).toString()) + "</td>";
             }
+            html += "</tr>";
         }
-    }**/
+        html += "</table></body></html>";
+        System.out.println();
+        System.out.println("The following is the HTML output. Paste it into a file and save as ptable.html and then open in a web browser.");
+        System.out.println("It is much easier to read than the text output.");
+        System.out.println();
+        System.out.print(html);
+    }
+    
+    // nonterminals like "<exp>" look like HTML tags, so we have to replace
+    // > and < with metacharacters &lt; and &gt;
+    private String fixHTML(String tag) {
+        return tag.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+    }
 }
