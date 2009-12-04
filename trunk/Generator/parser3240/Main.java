018be692d340e4462f34d96cb5d193816aa0bf56
mpnolan <mpnolan@341ec060-d5ea-11de-8017-1f6ee2e63914>
2009-12-04 19:08:39 +0000

    More work on parser driver.

--- trunk/Generator/parser3240/Main.java
+++ trunk/Generator/parser3240/Main.java
@@ -50,44 +50,68 @@ public class Main {
         ParserGenerator parserGen = new ParserGenerator();
         ILexer lexer = new Lexer();
         String fileContents = "";
-        ArrayList<Token> tokens = lexer.process(fileContents);
-                
+        ArrayList<Token> tokens = new ArrayList<Token>();
+        //EPSILON package body IDENT is separate SEMICOLON EPSILON
+        tokens.add(new Token("$"));
+        tokens.add(new Token("package"));
+        tokens.add(new Token("body"));
+        tokens.add(new Token("IDENT"));
+        tokens.add(new Token("is"));
+        tokens.add(new Token("separate"));
+        tokens.add(new Token("SEMICOLON"));
+        tokens.add(new Token("$"));
+        //tokens = lexer.process(fileContents);
+        
         String grammar = "";
-        parserGen.feed("C:\\tiny.ll1");
+        parserGen.feed("C:\\final grammar.ll1");
+        //parserGen.feed("C:\\louden_pg178.ll1");
         
         ParsingTable parseTable = parserGen.buildParsingTable();
-        parseTable.printTable();
+        //parseTable.printTable();
+        parseTable.printHTMLTable();
         
         // parser driver
         // see Pg 155 of Louden
 //        int i = 0;
 //        Stack<Symbol> stack = new Stack<Symbol>();
-//        // push start symbol onto the stack here
-//        // stack.push(parseTable.getStartSymbol());
+//        // push start symbol onto the stack
+//        stack.push(new Nonterminal(parserGen.getStartSymbol().getName()));
+//        int dep = 0;
 //        while (!stack.peek().equals(new Token("$")) && i < tokens.size()) {
+//            dep++;
+//            if (dep >= 10) break;
+//            System.out.print("stack: " + stack);
+//            // case 1: top of stack is a token
 //            if (stack.peek() instanceof Token) {
+//                System.out.println("Found token " + stack.peek() + " at top of stack.");
+//                // try to match token at top of stack with current input token
 //                if (stack.peek().equals(tokens.get(i))) {
 //                    stack.pop();
 //                    i++;
 //                }
-//                // else error???
+//                // tokens don't match, so parsing fails
+//                else {
+//                    break;
+//                }
 //            }
+//            // case 2: top of stack is a nonterminal
 //            // we have a nonterminal on top of the stack, so we expand it using
 //            // the parse table to push the next production onto the stack
 //            else if (stack.peek() instanceof Nonterminal) {
+//                System.out.println("Found nonterminal " + stack.peek() + " at top of stack.");
 //                ProductionRule rule = parseTable.getEntry((Nonterminal)stack.peek(), tokens.get(i));
+//                System.out.println("Next production is " + rule);
 //                // if parse table entry is empty, then ERROR
-//                //if (rule == null) break;
+//                if (rule == null) break;
 //                stack.pop();
 //                ArrayList<Symbol> symbols = rule.getRule();
 //                for (int x = symbols.size() - 1; x >= 0; x--)
 //                    stack.push(symbols.get(x));
-//                // else error???
 //            }
 //        }
 //        // we were able to consume the whole input, and we
 //        // have a $ at the top of the stack -- accept!
-//        if (stack.peek().equals("$") && i == tokens.size())
+//        if (stack.peek().equals(new Token("$")) && i == tokens.size())
 //            System.out.println("Successful parse!");
 //        else
 //            System.out.println("Parse error!");
